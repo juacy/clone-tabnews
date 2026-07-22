2aee749810619c6662d1635e0a6dc69727eca9bf
juacy <juacy987@gmail.com>
2026-07-21 22:29:48 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Repositório utilizado para treinar programação
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Se você é goiabinha, dá uma risadinha 🍈🍃</h1>
+function Home() {
+  return <h1>Se você é goiabinha, dá uma risadinha 🍈🍃</h1>;
 }
 
 export default Home;
